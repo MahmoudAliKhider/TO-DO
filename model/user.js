5d5ffc9fbccde8b5a1c16e39b4b0638185e872af
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-08-17 18:24:13 +0200

    hash password

--- model/user.js
+++ model/user.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
     name:String,
     email:{type:String,require:true},
     password:{ type : String , require: true}
 })
+userSchema.pre('save', function(next){
+    //generate salt value
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err){
+            return nodeTest(err)
+        }
+
+        //use this salt value to hash password
+        bcrypt.hash(this.password,salt,(err,hash)=>{
+           if(err){
+            return next(error)
+           }
+           this.password=hash;
+           next();
+        })
+    })
+})
+
 module.exports=mongoose.model('User',userSchema)
