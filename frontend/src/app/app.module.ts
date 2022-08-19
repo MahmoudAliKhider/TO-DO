f7a02cbe039eb1516876ce5069cc9faacf228cef
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-08-19 18:32:26 +0200

    Add navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
+import {  HttpClientModule } from '@angular/common/http';
+
+const AppRoutes :Routes=[
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(AppRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
